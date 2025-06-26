d6f4df21c98b04cf932ca111163d1aa47afa87d4
dp287517 <Daniel.x.palha@haleon.com>
2025-06-26 06:18:41 +0200

    probleme responsive

--- server.js
+++ server.js
@@ -1038,7 +1038,7 @@ app.delete('/api/tableaux/:id', async (req, res) => {
         }
         // Supprimer le tableau (les dépendances sont gérées par ON DELETE CASCADE)
         const result = await client.query('DELETE FROM tableaux WHERE id = $1 RETURNING *', [id]);
-        if (result.rows.length === 0) {
+        if (result.rowCount === 0) {
             console.log('[Server] Tableau non trouvé:', id);
             return res.status(404).json({ error: 'Tableau non trouvé' });
         }
