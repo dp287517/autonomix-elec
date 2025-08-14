175b3a0e09a4a3da8906d560ff9017fb1dad84d9
dp287517 <94993631+dp287517@users.noreply.github.com>
2025-08-14 06:04:43 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,12 @@
+// config/db.js
 const { Pool } = require('pg');
-
-const DATABASE_URL = process.env.DATABASE_URL;
-if (!DATABASE_URL) {
-  console.warn('[db] DATABASE_URL is not set');
+const connectionString = process.env.DATABASE_URL;
+let sslOption = undefined;
+if (process.env.PGSSLMODE && process.env.PGSSLMODE.toLowerCase() === 'disable') {
+  sslOption = false;
+} else {
+  sslOption = { rejectUnauthorized: false };
 }
-
-const ssl =
-  process.env.PGSSLMODE === 'disable'
-    ? false
-    : { rejectUnauthorized: false };
-
-const pool = new Pool({
-  connectionString: DATABASE_URL,
-  ssl,
-  max: parseInt(process.env.PG_POOL_MAX || '10', 10),
-  idleTimeoutMillis: 30_000,
-  connectionTimeoutMillis: 10_000,
-});
-
+const pool = new Pool({ connectionString, ssl: sslOption });
+pool.on('error', (err) => console.error('[DB] Pool error', err));
 module.exports = { pool };
