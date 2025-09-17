b533af49d72e792c42b8995d2b4467b63b0a58e6
dp287517 <94993631+dp287517@users.noreply.github.com>
2025-09-17 11:01:48 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,20 @@
 // config/db.js
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL, // Format Neon: postgres://user:password@neon-host/dbname
+  ssl: {
+    rejectUnauthorized: false // Nécessaire pour Neon
+  }
 });
+
+// Test connexion
+pool.connect((err) => {
+  if (err) {
+    console.error('[db] Erreur connexion Neon:', err);
+  } else {
+    console.log('[db] Connecté à Neon');
+  }
+});
+
 module.exports = { pool };
